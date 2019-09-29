a8e8e8ab4b3d3a9245eb38ebcb20007eb3e9fcc8
Ubuntu1996 <2287807181@qq.com>
2019-09-29 17:18:32 +0800

    add HITOJ_1868

---
+++ HITOJ_1868-Eight_digital/main.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <array>
+
+std::array<int, 9> raw;
+std::array<int, 9> target;
+
+int main(){
+	int t;
+	cin >> t;
+	while(t--){
+		for(int i=0; i<9; ++i){
+			cin >>
+		}
+	}
+}
