fc324c17a8db76c3cdf3edaf387ea0d23ed1beea
Ubuntu1996 <2287807181@qq.com>
2020-01-06 16:31:36 +0800

    add UVa_1584

---
+++ UVa_1584-Circular_Sequence/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cstring>
+#include <cfloat>
+#include <algorithm>
+#include <vector>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <map>
+#include <numeric>
+#include <climits>
+#include <cstring>
+#include <list>
+#include <queue>
+#include <utility>
+#include <functional>
+#include <unordered_map>
+#include <set>
+
+using std::queue;
+using std::pair;
+using std::make_pair;
+using std::priority_queue;
+using std::list;
+using std::map;
+using std::string;
+using std::vector;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::min;
+using std::max;
+using std::greater;
+using std::unordered_map;
+using std::set;
+using std::begin;
+using std::end;
+using std::find;
+
+int T;//number of cases
+string str;
+
+int main() {
+    cin >> T;
+    while (T--) {
+        cin >> str;
+        vector<string> res;
+
+        for (int i = 0; i < str.size(); ++i) {
+            string str_cir = str.substr(i, str.size()) + str.substr(0, i);
+            res.emplace_back(str_cir);
+        }
+
+        std::sort(res.begin(), res.end());
+        cout << *res.begin() << endl;
+    }
+}
