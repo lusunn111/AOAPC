01c50e9cc4b6a65a90d67c751d24638c1ca387e4
Ubuntu1996 <2287807181@qq.com>
2020-01-25 10:49:51 +0800

    update

--- UVa_1225-Digit_Counting/main.cpp
+++ UVa_1225-Digit_Counting/main.cpp
@@ -16,50 +16,54 @@
 #include <functional>
 #include <unordered_map>
 #include <set>
+#include <sstream>
+
+using std::cin;
+using std::cout;
+using std::endl;
 
 using std::queue;
 using std::pair;
-using std::make_pair;
 using std::priority_queue;
 using std::list;
 using std::map;
 using std::string;
 using std::vector;
-using std::cin;
-using std::cout;
-using std::endl;
-using std::min;
-using std::max;
-using std::greater;
 using std::unordered_map;
 using std::set;
-using std::begin;
-using std::end;
-using std::find;
+using std::stringstream;
 
-int T;//number of test cases
+using std::min;
+using std::max;
+
+int kases;
 int N;
 int count[10];
 
-void digitCount(int num) {
-    while (num) {
-        ++count[num % 10];
-        num /= 10;
+void init(){
+    memset(count, 0, sizeof(count));
+}
+
+//暴力法
+void digitCount() {
+    //O(n^2)
+    for (int i = 1; i <= N; ++i){
+        int i_copy = i;
+        while(i_copy){
+            ++count[i_copy%10];
+            i_copy /= 10;
+        }
     }
 }
 
 int main() {
-    cin >> T;
-    while (T--) {
+    cin >> kases;
+    while (kases--) {
         cin >> N;
-        memset(count, 0, sizeof(count));
-
-        for (int i = 1; i <= N; ++i) {
-            digitCount(i);
-        }
-
+        init();
+        digitCount();
         for (int i = 0; i < 9; ++i) {
-            cout << count[i] << ' ';
+            cout << count[i] << " ";
         }
         cout << count[9] << endl;
     }
